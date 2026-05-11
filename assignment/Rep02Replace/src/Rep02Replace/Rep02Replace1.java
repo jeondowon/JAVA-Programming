844bd6baebe2042d568bc212a0c8cede9400ea15
jeondowon <dowon.9102@gmail.com>
2026-05-11 22:28:48 +0900

    rep02

---
+++ assignment/Rep02Replace/src/Rep02Replace/Rep02Replace1.java
@@ -0,0 +1,18 @@
+package Rep02Replace;
+
+import java.util.Scanner;
+
+class Rep02Replace1 {
+    public static void main(String[] args){
+        Rep02Replace.Rep02Replace1 a2 = new Rep02Replace.Rep02Replace1();
+        a2.level1();
+    }
+    void level1(){
+        String line;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a line of text.");
+        line = sc.nextLine();
+
+        System.out.println("You entered: " + line);
+    }
+}
